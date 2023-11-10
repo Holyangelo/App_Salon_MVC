aee4be6fc1794bfe65a3a959707b082df5effe35
localhost <localhost.robot@gmail.com>
2023-11-10 19:02:49 -0400

    sql added

--- db/barbershop.sql
+++ db/barbershop.sql
@@ -30,7 +30,7 @@ CREATE TABLE `citas` (
   PRIMARY KEY (`id`),
   KEY `usuarioId` (`usuarioId`),
   CONSTRAINT `citas_ibfk_1` FOREIGN KEY (`usuarioId`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,6 +39,7 @@ CREATE TABLE `citas` (
 
 LOCK TABLES `citas` WRITE;
 /*!40000 ALTER TABLE `citas` DISABLE KEYS */;
+INSERT INTO `citas` VALUES (1,'2023-11-15','14:03:00',10),(2,'2023-11-15','14:03:00',10),(3,'2023-11-15','16:03:00',10),(4,'2023-11-15','16:05:00',10),(5,'2023-11-14','15:21:00',10),(6,'2023-11-15','16:27:00',10),(7,'2023-11-23','15:39:00',10),(8,'2023-11-16','15:41:00',10),(9,'2023-11-14','15:49:00',10),(10,'2023-11-14','15:49:00',10),(11,'2023-11-14','15:49:00',10),(12,'2023-11-14','15:49:00',10),(13,'2023-11-14','15:49:00',10),(14,'2023-11-14','15:49:00',10),(15,'2023-11-14','15:49:00',10),(16,'2023-11-14','15:49:00',10),(17,'2023-11-29','15:50:00',10),(18,'2023-11-14','15:51:00',10),(19,'2023-11-13','15:51:00',10),(20,'2023-11-14','15:56:00',10),(21,'2023-11-17','15:56:00',10),(22,'2023-11-13','16:48:00',10),(23,'2023-11-21','09:06:00',10),(24,'2023-11-14','16:18:00',10);
 /*!40000 ALTER TABLE `citas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -82,7 +83,7 @@ CREATE TABLE `servicios` (
   `nombre` varchar(60) NOT NULL,
   `precio` decimal(5,2) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -91,6 +92,7 @@ CREATE TABLE `servicios` (
 
 LOCK TABLES `servicios` WRITE;
 /*!40000 ALTER TABLE `servicios` DISABLE KEYS */;
+INSERT INTO `servicios` VALUES (1,'corte caballero',20.00),(2,'corte dama',30.00),(3,'corte niño',30.00);
 /*!40000 ALTER TABLE `servicios` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -123,7 +125,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES (1,' Osmant','Sallath','osmsallath@gmail.com','+584146441588',0,0,'6522fa8d07850','$2y$10$2y.1LN7JSy8qMdLrAF974OXsC20bHGrF5LYnEm2sBDZgMt95RKpV2','2023-10-08 14:53:01'),(2,' Osmant','Sallath','osmantsallath@gmail.com','+584146441588',0,0,'6522fdb6588a3','$2y$10$V7ZViJIyA3c4s.oHJXgDLuObAYnifKBUoRM39/Y3S1./MybL6KJXy','2023-10-08 15:06:30'),(3,' Osmant','Sallath','admin@realstate.com','+584146441588',0,0,'65230269d7ecb','$2y$10$8qelusg5SteYS1nH3pAI/.QH53WcsNvbEWtvmRIkHhAtpYPnpHyx6','2023-10-08 15:26:35'),(4,' Osmant','Sallath','tcdprotocol@gmail.com','+584146441588',0,0,'6523037713767','$2y$10$oNIePi8Zh3Lno5E9NZt3C.oVGQUnJ1P9S742r421Tu4hpuJJZgiKS','2023-10-08 15:31:04'),(5,' Osmant','Sallath','deezerpacc@gmail.com','+584146441588',0,0,'652307304661d','$2y$10$L5ngQD3aUpdoYfKAjVIbNOqVW7ke/K27MTDS52jYagVKOBTN8cv2C','2023-10-08 15:46:58'),(6,' Osmant','Sallath','mydreamsmusicmind@gmail.com','+584146441588',0,0,'6523077dde2ca','$2y$10$wSmQeUAQcrO/0bf1Zfp5cOPa.6oi4qTbDwbtLyzeyYmKiLt5Rs3le','2023-10-08 15:48:15'),(7,' Osmant','Sallath','osmurdaneta@gmail.com','+584146441588',0,0,'65230bc3e2ecd','$2y$10$GaGgIxehTkU.huWwotTK3eo5aA9G3HjWWB/a0Ofl6INtd9gRduDKq','2023-10-08 16:06:29'),(8,' Osmant','Sallath','ignistergaming@gmail.com','+584146441588',0,0,'65230c266a520','$2y$10$zy/8DVvcl5PaMM2DSL3WO.hCg7uNv.PoDgJGDjCFlhuGKLI6eep3O','2023-10-08 16:08:08'),(9,' Osmant','Sallath','dreamsofdeveloper@gmail.com','+584146441588',0,1,'','$2y$10$.NdUIS2d5bK6ou7cwK6ySOT8qdn7TSqzo9Cziy06HDds5suTjCKW2','2023-10-08 16:09:36'),(10,' Osmant','Sallath','ignistechcorp@gmail.com','+584146441588',0,1,NULL,'$2y$10$elTQbTgb7BDANXfvu/V1pOVu/MHwy89AqrPAM/LQUtefe/Dov/VxK','2023-10-08 16:38:57'),(11,' Osmant','Sallath','archangelsariel1992@gmail.com','+584146441588',0,1,'','$2y$10$KcfX2RvSBHaaI9df.bg1juRZugZnFiYaafRZ0fqtb2iELDHOLRbFq','2023-10-08 17:26:32');
+INSERT INTO `usuarios` VALUES (1,' Osmant','Sallath','osmsallath@gmail.com','+584146441588',0,0,'6522fa8d07850','$2y$10$2y.1LN7JSy8qMdLrAF974OXsC20bHGrF5LYnEm2sBDZgMt95RKpV2','2023-10-08 14:53:01'),(2,' Osmant','Sallath','osmantsallath@gmail.com','+584146441588',0,0,'6522fdb6588a3','$2y$10$V7ZViJIyA3c4s.oHJXgDLuObAYnifKBUoRM39/Y3S1./MybL6KJXy','2023-10-08 15:06:30'),(3,' Osmant','Sallath','admin@realstate.com','+584146441588',0,0,'65230269d7ecb','$2y$10$8qelusg5SteYS1nH3pAI/.QH53WcsNvbEWtvmRIkHhAtpYPnpHyx6','2023-10-08 15:26:35'),(4,' Osmant','Sallath','tcdprotocol@gmail.com','+584146441588',0,0,'6523037713767','$2y$10$oNIePi8Zh3Lno5E9NZt3C.oVGQUnJ1P9S742r421Tu4hpuJJZgiKS','2023-10-08 15:31:04'),(5,' Osmant','Sallath','deezerpacc@gmail.com','+584146441588',0,0,'652307304661d','$2y$10$L5ngQD3aUpdoYfKAjVIbNOqVW7ke/K27MTDS52jYagVKOBTN8cv2C','2023-10-08 15:46:58'),(6,' Osmant','Sallath','mydreamsmusicmind@gmail.com','+584146441588',0,1,'','$2y$10$wSmQeUAQcrO/0bf1Zfp5cOPa.6oi4qTbDwbtLyzeyYmKiLt5Rs3le','2023-10-08 15:48:15'),(7,' Osmant','Sallath','osmurdaneta@gmail.com','+584146441588',0,0,'65230bc3e2ecd','$2y$10$GaGgIxehTkU.huWwotTK3eo5aA9G3HjWWB/a0Ofl6INtd9gRduDKq','2023-10-08 16:06:29'),(8,' Osmant','Sallath','ignistergaming@gmail.com','+584146441588',1,1,'','$2y$10$zy/8DVvcl5PaMM2DSL3WO.hCg7uNv.PoDgJGDjCFlhuGKLI6eep3O','2023-10-08 16:08:08'),(9,' Osmant','Sallath','dreamsofdeveloper@gmail.com','+584146441588',0,1,'','$2y$10$.NdUIS2d5bK6ou7cwK6ySOT8qdn7TSqzo9Cziy06HDds5suTjCKW2','2023-10-08 16:09:36'),(10,' Osmant','Sallath','ignistechcorp@gmail.com','+584146441588',0,1,'','$2y$10$EEcGNnbwvLI3L83Y1GfjcOXVFIAVbEDakqKRKQ.fdnjPz3GjCCmnG','2023-10-08 16:38:57'),(11,' Osmant','Sallath','archangelsariel1992@gmail.com','+584146441588',0,1,'','$2y$10$KcfX2RvSBHaaI9df.bg1juRZugZnFiYaafRZ0fqtb2iELDHOLRbFq','2023-10-08 17:26:32');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -140,4 +142,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-10-08 18:46:41
+-- Dump completed on 2023-11-10 19:01:21
