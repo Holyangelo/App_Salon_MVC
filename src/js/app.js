f2668a9032efad70e113c29c795b70013ee82f9b
localhost <localhost.robot@gmail.com>
2023-11-10 15:55:28 -0400

    fetch try catch

--- src/js/app.js
+++ src/js/app.js
@@ -384,31 +384,35 @@ const reservarCita = async () => {
 	data.append("hora", hora);
 	data.append("fecha", fecha);
 	data.append("servicios", idServicios);
-	const options = {
-		method: "POST", // *GET, POST, PUT, DELETE, etc.
-		/*mode: "cors", // no-cors, *cors, same-origin
-		cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-		//credentials: "same-origin", // include, *same-origin, omit
-		headers: {
-			"Content-Type": "application/json",
-			// 'Content-Type': 'application/x-www-form-urlencoded',
-		},
-		//redirect: "follow", // manual, *follow, error
-		//referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-		*/
-		body: data // body data type must match "Content-Type" header
-	}
+	try {
+		const options = {
+			method: "POST", // *GET, POST, PUT, DELETE, etc.
+			/*mode: "cors", // no-cors, *cors, same-origin
+			cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+			//credentials: "same-origin", // include, *same-origin, omit
+			headers: {
+				"Content-Type": "application/json",
+				// 'Content-Type': 'application/x-www-form-urlencoded',
+			},
+			//redirect: "follow", // manual, *follow, error
+			//referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+			*/
+			body: data // body data type must match "Content-Type" header
+		}
 
-	const response = await fetch(url, options);
-	const resultado = await response.json();
-	if (resultado.resultado === true) {
-		mostrarAlerta(true, "Cita Reservada");
-		const btn = document.querySelector(".boton");
-		btn.remove();
-		//btn.setAttribute("disabled", "");
-		setTimeout(() => {
-			window.location.reload();
-		}, 3000);
+		const response = await fetch(url, options);
+		const resultado = await response.json();
+		if (resultado.resultado === true) {
+			mostrarAlerta(true, "Cita Reservada");
+			const btn = document.querySelector(".boton");
+			btn.remove();
+			//btn.setAttribute("disabled", "");
+			setTimeout(() => {
+				window.location.reload();
+			}, 3000);
+		}
+	} catch (error) {
+		console.log(error);
 	}
 }
 
